ada4a0ed92618df53a0b2cedf8e0a3541be3383f
César Bartolomé Navarro <cesarbartolomenavarro2399@gmail.com>
2023-07-18 17:32:23 +0100

    Creados los modelos

---
+++ Models/Imagen.cs
@@ -0,0 +1,12 @@
+﻿namespace PinturasAPI.Models
+{
+    public class Imagen
+    {
+        public int Id { get; set; }
+
+        public byte[] ImgData { get; set; }
+        public string Descripcion { get; set; }
+
+        public Tipo tipo { get; set; }
+    }
+}
---
+++ Models/Tipo.cs
@@ -0,0 +1,10 @@
+﻿namespace PinturasAPI.Models
+{
+    public class Tipo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<Imagen> Imagenes { get; set;}
+    }
+}
