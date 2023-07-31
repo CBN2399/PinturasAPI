44a8ef44aee364d6a04ac303ae29f2cd3a9fd01d
César Bartolomé Navarro <cesarbartolomenavarro2399@gmail.com>
2023-07-31 13:30:37 +0100

    Modificado modelo imagen y añadidad fotos de prueba

--- Models/Imagen.cs
+++ Models/Imagen.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public byte[] ImgData { get; set; }
+        public string Name { get; set; }
         public string Descripcion { get; set; }
 
         public Tipo tipo { get; set; }
